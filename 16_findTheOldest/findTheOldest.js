65829823be6da613517e7a913527f4516eb75734
Akerlox <125981924+Akerlox54@users.noreply.github.com>
2026-07-24 21:42:02 +0700

    Finished.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(ppl) {
+    const currentYear = new Date().getFullYear();
+    let sortedByAge = ppl.sort((a, b) => {
+        if ( a.yearOfDeath == undefined) {
+            return -((currentYear - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+        }
 
+        else if (b.yearOfDeath == undefined) {
+            return -((a.yearOfDeath - a.yearOfBirth) - (currentYear - b.yearOfBirth));
+        }
+
+        else if (a.yearOfDeath == undefined && b.yearOfDeath == undefined) {
+            return -((currentYear - a.yearOfBirth) - (currentYear - b.yearOfBirth));
+        }
+        
+        else return -((a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+    });
+    return sortedByAge[0];
 };
 
 // Do not edit below this line
